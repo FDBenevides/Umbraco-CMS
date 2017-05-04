775b667f6d80df76d93f4503b14b2693dd95f050
fdpb <fdpb@WORKPC>
2017-05-04 12:05:26 +0100

    added custom grid editors (skybrudimagepicker, simpleparagraph) in App_Plugins/CustomGridEditors/package.manifest; added new partials views for SkybrudImagePicker grid editor;

---
+++ src/Umbraco.Web.UI/App_Plugins/CustomGridEditors/Paragraph/Controllers/Paragraph.js
@@ -0,0 +1,13 @@
+﻿angular.module("umbraco")
+    .controller("CustomGridEditors.ParagraphController",
+    function ($scope) {
+
+        //// Since Umbraco will strip all $ properties from the model on save, we need to create a "shadow" object
+        //$scope.selected = $scope.model.value ? angular.copy($scope.model.value) : null;
+
+        //// And when the "shadow" object changes, we update the model value
+        //$scope.$watch('selected', function () {
+        //    $scope.model.value = $scope.selected;
+        //}, true);
+
+    });
